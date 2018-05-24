f2801782c50937f151051a3f69a5f2cb5b91d9ca
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-05-24 13:16:17 +0800

    mmp

--- 拯救007.cpp
+++ 拯救007.cpp
@@ -10,16 +10,18 @@ point zero;
 int FirstJump(point pnt);
 int dfs(point p);
 int IsSave(point p);
-double dis(point a,point b)	{return sqrt((a.x-b.x)*(a.x-b.x)+(a.y-b.y)*(a.y-b.y));}
+double dis(point a,point b)	{
+	return sqrt((a.x-b.x)*(a.x-b.x)+(a.y-b.y)*(a.y-b.y));
+}
 int n,len;
+double rad=15.0/2;
 int main()	{
 	zero.x=0,zero.y=0;
 	int ans=0;
 	scanf("%d %d",&n,&len);
 	for (int i=0;i<n;i++)	scanf("%lf %lf",&coord[i].x,&coord[i].y);
 	for (int i=0;i<n;i++)	{
-		cout << "Test" <<endl;
-		if (!FirstJump(coord[i])&&!spot[i]){
+		if (FirstJump(coord[i])&&!spot[i]){
 			spot[i]=1;
 			ans=dfs(coord[i]);
 			if (ans)	break;
@@ -30,13 +32,16 @@ int main()	{
 	return 0;
 }
 int FirstJump(point pnt)	{
-	if (dis(pnt,zero)>len+15)	return 0;
+	if (dis(pnt,zero)>len+rad)	return 0;
 	else return 1;
 }
 int dfs(point p)	{
 	if (IsSave(p))	return 1;
 	for (int i=0;i<n;i++)
-		if (!spot[i]&&dis(p,coord[i])<=len)	dfs(coord[i]);
+		if (!spot[i]&&dis(p,coord[i])<=len){
+			printf("%.2f",dis(p,coord[i]));
+			dfs(coord[i]);
+		}
 	return 0;
 }
 int IsSave(point p)	{
